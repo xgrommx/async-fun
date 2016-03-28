3e1cc1f05bdab87055b3faf0db6dddbe51203d93
Denis Stoyanov <stoyanov.gr@gmail.com>
2016-03-29 02:23:38 +0300

    The first commit.

---
+++ transform-async-to-module-method/rx/src/index.js
@@ -0,0 +1,9 @@
+const asyncFn = async (a, b, c) => {
+    const x = await Promise.resolve(a);
+    const y = await Promise.resolve(b);
+    const z = await Promise.resolve(c);
+
+    return x + y + z;
+};
+
+asyncFn(10, 20, 30).subscribe(sum => console.log(`Sum = ${sum}`));
